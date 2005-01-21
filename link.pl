25b19dd3538289cdda4490ffc0030671f858e019
Martin Edenhofer <martin.edenhofer@otrs.com>
2005-01-21 12:59:21 +0000

    fixed file rename

--- link.pl
+++ link.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# $Id: link.pl,v 1.4 2005-01-21 09:55:47 martin Exp $
+# $Id: link.pl,v 1.5 2005-01-21 12:59:21 martin Exp $
 
 use strict;
 
@@ -32,24 +32,27 @@ sub R {
             $File =~ s/$Start//;
 #            print "File: $File\n";
 #            my $Dir =~ s/^(.*)\//$1/;
-          if ($File !~ /Entries|Repository|Root|CVS/) {
-            if (!-e"$Dest/$File" || (-l "$Dest/$File" && unlink ("$Dest/$File"))) {
+            if ($File !~ /Entries|Repository|Root|CVS/) {
+#            if (!-e"$Dest/$File" || (-l "$Dest/$File" && unlink ("$Dest/$File"))) {
+                if (-l "$Dest/$File" && unlink ("$Dest/$File")) {
+                    unlink ("$Dest/$File") || die "ERROR: Can't unlink symlink: $Dest/$File";
+                }
+                if (-e "$Dest/$File") {
+                    if (! rename("$Dest/$File", "$Dest/$File.old")) {
+                        print "NOTICE: Backup orig file: $Dest/$File.old";
+                    }
+                    else {
+                        die "ERROR: Can't rename $Dest/$File to $Dest/$File.old: $!";
+                    }
+                }
                 if (!-e $Dest) {
                     die "ERROR: No such directory: $Dest";
                 }
                 elsif (!-e $OrigFile) {
                     die "ERROR: No such orig file: $OrigFile";
                 }
-                elsif (-e "$Dest/$File") {
-                    if (! rename("$Dest/$File", "$Dest/$File.old")) {
-                        print "NOTICE: Backup orig file: $Dest/$File.old";
-                    }
-                    {
-                        die "ERROR: Can't rename $Dest/$File to $Dest/$File.old: $!";
-                    }
-                }
                 elsif (!symlink ($OrigFile, "$Dest/$File")) {
-#                    die "ERROR: Can't link ($OrigFile->$Dest/$File): $!";
+#                        die "ERROR: Can't link ($OrigFile->$Dest/$File): $!";
                     die "ERROR: Can't $File link: $!";
                 }
                 else {
@@ -57,11 +60,6 @@ sub R {
                     print "NOTICE:       $Dest/$File\n";
                 }
             }
-            elsif (-e "$Dest/$File") {
-                die "ERROR: Can't link, file already exists: $Dest/$File";
-            }
-          }
-#            system ("");
         }
     }
 }
