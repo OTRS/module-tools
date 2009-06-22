d7d322ed7b311c12e27115e52df95dcd6736a7bb
Martin Gruner <martin.gruner@otrs.com>
2009-06-22 13:24:16 +0000

    Fixed CVS directory exclusion and typo. My first commit at ((otrs))! Yay!

--- link.pl
+++ link.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# $Id: link.pl,v 1.10 2007-05-30 07:36:42 tr Exp $
+# $Id: link.pl,v 1.11 2009-06-22 13:24:16 mg Exp $
 
 use strict;
 
@@ -22,60 +22,57 @@ sub R {
     my @List = glob("$In/*");
     foreach my $File (@List) {
         $File =~ s/\/\//\//g;
-        if (-d $File && $File !~ /^\/CVS$/) {
-            R($File);
-            $File =~ s/$Start//;
-#            print "Directory: $File\n";
+        # recurse into subdirectories
+        if (-d $File) {
+            # skip CVS directories
+            if ($File !~ /\/CVS$/) {
+                R($File);
+            }
         }
         else {
             my $OrigFile = $File;
             $File =~ s/$Start//;
-#            print "File: $File\n";
-#            my $Dir =~ s/^(.*)\//$1/;
-            if ($File !~ /\/(Entries|Repository|Root|Tag|^CVS$)/ && $File !~ /^(Entries|Repository|Root|Tag|^CVS$)/) {
-                # check directory of loaction (in case create a directory)
-                if ("$Dest/$File" =~ /^(.*)\/(.+?|)$/) {
-                    my $Directory = $1;
-                    my @Directories = split(/\//, $Directory);
-                    my $DirectoryCurrent = '';
-                    foreach my $Directory (@Directories) {
-                        $DirectoryCurrent .= "/$Directory";
-                        if ($DirectoryCurrent && ! -d $DirectoryCurrent) {
-                            if (mkdir $DirectoryCurrent) {
-                                print STDERR "NOTICE: Create Directory $DirectoryCurrent\n";
-                            }
-                            else {
-                                die "ERROR: can't create directory $DirectoryCurrent: $!";
-                            }
+            # check directory of location (in case create a directory)
+            if ("$Dest/$File" =~ /^(.*)\/(.+?|)$/)
+            {
+                my $Directory = $1;
+                my @Directories = split(/\//, $Directory);
+                my $DirectoryCurrent = '';
+                foreach my $Directory (@Directories) {
+                    $DirectoryCurrent .= "/$Directory";
+                    if ($DirectoryCurrent && ! -d $DirectoryCurrent) {
+                        if (mkdir $DirectoryCurrent) {
+                            print STDERR "NOTICE: Create Directory $DirectoryCurrent\n";
+                        }
+                        else {
+                            die "ERROR: can't create directory $DirectoryCurrent: $!";
                         }
                     }
                 }
-#            if (!-e"$Dest/$File" || (-l "$Dest/$File" && unlink ("$Dest/$File"))) {
-                if (-l "$Dest/$File") {
-                    unlink ("$Dest/$File") || die "ERROR: Can't unlink symlink: $Dest/$File";
-                }
-                if (-e "$Dest/$File") {
-                    if (rename("$Dest/$File", "$Dest/$File.old")) {
-                        print "NOTICE: Backup orig file: $Dest/$File.old\n";
-                    }
-                    else {
-                        die "ERROR: Can't rename $Dest/$File to $Dest/$File.old: $!";
-                    }
-                }
-                if (!-e $Dest) {
-                    die "ERROR: No such directory: $Dest";
-                }
-                elsif (!-e $OrigFile) {
-                    die "ERROR: No such orig file: $OrigFile";
-                }
-                elsif (!symlink ($OrigFile, "$Dest/$File")) {
-#                        die "ERROR: Can't link ($OrigFile->$Dest/$File): $!";
-                    die "ERROR: Can't $File link: $!";
+            }
+            if (-l "$Dest/$File") {
+                unlink ("$Dest/$File") || die "ERROR: Can't unlink symlink: $Dest/$File";
+            }
+            if (-e "$Dest/$File") {
+                if (rename("$Dest/$File", "$Dest/$File.old")) {
+                    print "NOTICE: Backup orig file: $Dest/$File.old\n";
                 }
                 else {
-                    print "NOTICE: Link: $OrigFile -> \n";
-                    print "NOTICE:       $Dest/$File\n";
+                    die "ERROR: Can't rename $Dest/$File to $Dest/$File.old: $!";
                 }
+            }
+            if (!-e $Dest) {
+                die "ERROR: No such directory: $Dest";
+            }
+            elsif (!-e $OrigFile) {
+                die "ERROR: No such orig file: $OrigFile";
+            }
+            elsif (!symlink ($OrigFile, "$Dest/$File")) {
+                die "ERROR: Can't $File link: $!";
+            }
+            else {
+                print "NOTICE: Link: $OrigFile -> \n";
+                print "NOTICE:       $Dest/$File\n";
             }
         }
     }
