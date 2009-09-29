77484ee9ac2ae60cf2e522dccfe89655f435dae0
Karl Kleinpaste <karl.kleinpaste@otrs.com>
2009-09-29 10:32:04 +0000

    Additions for diff options.

--- module_check.pl
+++ module_check.pl
@@ -3,7 +3,7 @@
 # module-tools/module_check.pl - script to check OTRS modules
 # Copyright (C) 2001-2009 OTRS AG, http://otrs.org/
 # --
-# $Id: module_check.pl,v 1.10 2009-07-15 16:57:11 bes Exp $
+# $Id: module_check.pl,v 1.11 2009-09-29 10:32:04 kk Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -27,7 +27,7 @@ module_check.pl - script to check OTRS modules
 
 =head1 SYNOPSIS
 
-module_check.pl -o <Original-Framework-Path> -m <Module-Path> -v [verbose mode] -d [debug mode 1]
+module_check.pl -o <Original-Framework-Path> -m <Module-Path> -v [verbose mode] -d [debug mode 1] [diff options: -u|-b|-B]
 
 =head1 DESCRIPTION
 
@@ -48,7 +48,7 @@ use File::Find;
 use File::Temp qw( tempfile );
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.10 $) [1];
+$VERSION = qw($Revision: 1.11 $) [1];
 
 # get options
 my %Opts = ();
@@ -61,7 +61,7 @@ if (!$Opts{'o'} || !$Opts{'m'} ) {
 if ( $Opts{'h'} ) {
     print "module_check.pl <Revision $VERSION> - Check OTRS modules\n";
     print "Copyright (C) 2001-2009 OTRS AG, http://otrs.org/\n";
-    print "usage: module_check.pl -o <Original-Framework-Path> -m <Module-Path> -v [verbose mode] -d [debug mode 1]\n\n";
+    print "usage: module_check.pl -o <Original-Framework-Path> -m <Module-Path> -v [verbose mode] -d [debug mode 1] [diff options: -u|-b|-B]\n\n";
     exit 1;
 }
 
@@ -125,8 +125,16 @@ sub CheckFile {
     print $ModuleFH $ModuleContent;
     print $OriginalFH $OriginalContent;
 
+    # process diff options
+    my $DiffOptions = '';
+    for my $Opt ( qw(u b B) ) {
+        if ( defined( $Opts{$Opt} ) ) {
+            $DiffOptions .= " -$Opt";
+        }
+    }
+
     # make a diff of the content
-    my $DiffResult = `diff $ModuleTempfile $OriginalTempfile`;
+    my $DiffResult = `diff $DiffOptions $ModuleTempfile $OriginalTempfile`;
 
     # print diff result
     if ( $Opts{'v'} || $DiffResult ) {
@@ -272,16 +280,16 @@ sub ContentClean {
 
     # delete the different version lines
 
-    # example1: $VERSION = qw($Revision: 1.10 $) [1];
+    # example1: $VERSION = qw($Revision: 1.11 $) [1];
     $Content =~ s{ ^ \$VERSION [ ] = [ ] qw \( \$[R]evision: [ ] .+? $ }{}ixms;
 
-    # example2: $VERSION = '$Revision: 1.10 $';
+    # example2: $VERSION = '$Revision: 1.11 $';
     $Content =~ s{ ^ \$VERSION [ ] = [ ] '     \$[R]evision: [ ] .+? $ }{}ixms;
 
     # example3:
     #=head1 VERSION
     #
-    #$Revision: 1.10 $ $Date: 2009-07-15 16:57:11 $
+    #$Revision: 1.11 $ $Date: 2009-09-29 10:32:04 $
     #
     #=cut
     $Content =~ s{
