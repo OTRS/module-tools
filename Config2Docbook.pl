b5eea0567d73708a9f9098d82af4046c5f4c58b2
Thorsten Eckel <thorsten.eckel@otrs.com>
2012-07-09 15:21:29 +0000

    Fixed bug: 'Can't call method "Log" on an undefined value at Config2Docbook.pl line 550.'.

--- Config2Docbook.pl
+++ Config2Docbook.pl
@@ -3,7 +3,7 @@
 # Config2Docbook.pl - rebuild config
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: Config2Docbook.pl,v 1.1 2012-06-12 14:03:52 cr Exp $
+# $Id: Config2Docbook.pl,v 1.2 2012-07-09 15:21:29 te Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -64,7 +64,7 @@ use warnings;
 use File::Basename;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.1 $) [1];
+$VERSION = qw($Revision: 1.2 $) [1];
 
 use vars (qw($Self));
 use Getopt::Std;
@@ -547,10 +547,7 @@ sub _FileWrite {
     # return if file can not open
     my $FH;
     if ( !open $FH, $Mode, $Param{Location} ) {
-        $Self->{LogObject}->Log(
-            Priority => 'error',
-            Message  => "Can't write '$Param{Location}': $!",
-        );
+        print STDERR "ERROR: Can't write '$Param{Location}': $!";
         return;
     }
 
