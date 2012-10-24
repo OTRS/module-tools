9e1ac904894ec4c0732cf46d47b6ef76de8bc7d7
Michiel Beijen <michiel.beijen@otrs.com>
2012-10-24 08:52:25 +0000

    Added DatabaseInstall script to help creating database tables for linked modules.

---
+++ DatabaseInstall.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl -w
+# --
+# bin/DatabaseInstall.pl - to install the packages DatabaseInstall()
+# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# --
+# $Id: DatabaseInstall.pl,v 1.1 2012-10-24 08:52:25 mb Exp $
+# --
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU AFFERO General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# or see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+use strict;
+use warnings;
+
+# use ../ as lib location
+use File::Basename;
+use FindBin qw($RealBin);
+use lib dirname($RealBin);
+use lib dirname($RealBin) . "/Kernel/cpan-lib";
+
+use Getopt::Std;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.1 $) [1];
+
+use Kernel::Config;
+use Kernel::System::Encode;
+use Kernel::System::Log;
+use Kernel::System::Main;
+use Kernel::System::DB;
+use Kernel::System::Time;
+use Kernel::System::Package;
+
+# call the script with the module name as first argument
+my $Module = shift;
+if ( !-e $Module ) {
+    print "Can not find file $Module!\n";
+    exit 0;
+}
+
+# create common objects
+my %CommonObject = ();
+$CommonObject{ConfigObject} = Kernel::Config->new();
+$CommonObject{EncodeObject} = Kernel::System::Encode->new(%CommonObject);
+$CommonObject{LogObject}    = Kernel::System::Log->new(
+    LogPrefix    => "OTRS-$Module",
+    ConfigObject => $CommonObject{ConfigObject},
+);
+$CommonObject{MainObject}    = Kernel::System::Main->new(%CommonObject);
+$CommonObject{DBObject}      = Kernel::System::DB->new(%CommonObject);
+$CommonObject{TimeObject}    = Kernel::System::Time->new(%CommonObject);
+$CommonObject{XMLObject}     = Kernel::System::XML->new(%CommonObject);
+$CommonObject{PackageObject} = Kernel::System::Package->new(%CommonObject);
+
+my $PackageContent = $CommonObject{MainObject}->FileRead(
+    Directory => '.',
+    Filename  => $Module,
+);
+
+my %Structure = $CommonObject{PackageObject}->PackageParse( String => $PackageContent );
+
+if ( $Structure{DatabaseInstall} && $Structure{DatabaseInstall}->{post} ) {
+    $CommonObject{PackageObject}->_Database( Database => $Structure{DatabaseInstall}->{post} );
+}
+
+print "... done\n"
