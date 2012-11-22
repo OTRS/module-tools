b690550e87852e7ae11ba2582d42d3fe5d0b90b0
Udo Bretz <udo.bretz@otrs.com>
2012-11-22 07:58:02 +0000

    Improved handling to be more similar to the DatabaseInstall script.

--- CodeInstall.pl
+++ CodeInstall.pl
@@ -3,7 +3,7 @@
 # bin/CodeInstall.pl - to install the packagesetup CodeInstall()
 # Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
 # --
-# $Id: CodeInstall.pl,v 1.2 2012-11-20 19:16:41 mh Exp $
+# $Id: CodeInstall.pl,v 1.3 2012-11-22 07:58:02 ub Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -33,7 +33,7 @@ use lib dirname($RealBin) . "/Kernel/cpan-lib";
 use Getopt::Std;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.2 $) [1];
+$VERSION = qw($Revision: 1.3 $) [1];
 
 use Kernel::Config;
 use Kernel::System::Encode;
@@ -41,10 +41,14 @@ use Kernel::System::Log;
 use Kernel::System::Main;
 use Kernel::System::DB;
 use Kernel::System::Time;
-use Kernel::System::XML;
+use Kernel::System::Package;
 
 # call the script with the module name as first argument
 my $Module = shift;
+if ( !-e $Module ) {
+    print "Can not find file $Module!\n";
+    exit 0;
+}
 
 # create common objects
 my %CommonObject = ();
@@ -54,21 +58,25 @@ $CommonObject{LogObject}    = Kernel::System::Log->new(
     LogPrefix    => "OTRS-$Module",
     ConfigObject => $CommonObject{ConfigObject},
 );
-$CommonObject{MainObject} = Kernel::System::Main->new(%CommonObject);
-$CommonObject{DBObject}   = Kernel::System::DB->new(%CommonObject);
-$CommonObject{TimeObject} = Kernel::System::Time->new(%CommonObject);
-$CommonObject{XMLObject}  = Kernel::System::XML->new(%CommonObject);
+$CommonObject{MainObject}    = Kernel::System::Main->new(%CommonObject);
+$CommonObject{DBObject}      = Kernel::System::DB->new(%CommonObject);
+$CommonObject{TimeObject}    = Kernel::System::Time->new(%CommonObject);
+$CommonObject{XMLObject}     = Kernel::System::XML->new(%CommonObject);
+$CommonObject{PackageObject} = Kernel::System::Package->new(%CommonObject);
 
-# create the package name
-my $CodeModule = 'var::packagesetup::' . $Module;
+my $PackageContent = $CommonObject{MainObject}->FileRead(
+    Directory => '.',
+    Filename  => $Module,
+);
 
-# load the module
-if ( $CommonObject{MainObject}->Require($CodeModule) ) {
+my %Structure = $CommonObject{PackageObject}->PackageParse( String => $PackageContent );
 
-    # create new instance
-    $CommonObject{CodeObject} = $CodeModule->new(%CommonObject);
+if ( $Structure{CodeInstall} ) {
+    $CommonObject{PackageObject}->_Code(
+        Code      => $Structure{CodeInstall},
+        Type      => 'post',
+        Structure => \%Structure,
+    );
 }
 
-print "Run CodeInstall()\n";
-$CommonObject{CodeObject}->CodeInstall();
 print "... done\n"
