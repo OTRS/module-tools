4a0f3495e7fbd3030e206ce4b3f3eb68d4a8410f
Stefan Bedorf <stefan.bedorf@otrs.com>
2013-08-19 16:02:14 +0200

    Revertes previous change (use 'section' again).

--- Config2Docbook.pl
+++ Config2Docbook.pl
@@ -41,11 +41,11 @@ sub sorted_keys
     if ( $Name eq 'chapter' ) {
 
         # set the right sort order
-        return ( 'title', 'para', 'sect1', );
+        return ( 'title', 'para', 'section', );
     }
 
     # only change sort order for section
-    if ( $Name eq 'sect1' ) {
+    if ( $Name eq 'section' ) {
 
         # set the right sort order
         return ( 'title', 'para', );
@@ -291,7 +291,7 @@ sub _CreateDocbookConfigChapter {
             title => 'Configuration',
             para =>
                 'The package can be configured via the SysConfig in the Admin Interface. The following configuration options are available:',
-            sect1 => [],
+            section => [],
         },
     );
 
@@ -353,7 +353,7 @@ sub _CreateDocbookConfigChapter {
         }
     }
 
-    $Docbook{chapter}->{sect1} = \@ConvertedSettings;
+    $Docbook{chapter}->{section} = \@ConvertedSettings;
 
     #output
     print "\nGenerating Docbook structure...";
