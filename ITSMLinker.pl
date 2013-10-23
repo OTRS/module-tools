c3744ec0098817ad2eca79974ccbebcc8f4a01f2
Udo Bretz <udo.bretz@otrs.com>
2013-10-23 14:10:19 +0200

    Added -d option to also execute DatabaseInstall and CodeInstall on installation.

--- ITSMLinker.pl
+++ ITSMLinker.pl
@@ -40,10 +40,11 @@ ITSMLinker.pl -  to link / unlink all ITSM modules into a OTRS system
 Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
 
 Usage:
-    ITSMLinker.pl -a <install|uninstall> -v <ITSM branch version number> -m <Module-Path> -o <OTRS-path>
+    ITSMLinker.pl -a <install|uninstall> -v <ITSM branch version number> -m <Module-Path> -o <OTRS-path> [ -d (also executes DatabaseInstall and CodeInstall) ]
 
-Example:
+Examples:
     ITSMLinker.pl -a install -v 3.3 -m /devel -o /devel/otrs33-itsm
+    ITSMLinker.pl -a install -v 3.3 -m /devel -o /devel/otrs33-itsm -d
 
 EOF
 
@@ -60,6 +61,7 @@ my @ITSMModules = qw(
     ImportExport
 );
 
+# reverse the list of packages for uninstall
 if ( $Opts{'a'} eq 'uninstall' ) {
     @ITSMModules = reverse @ITSMModules;
 }
@@ -71,11 +73,26 @@ $Opts{'v'} = '_' . $Opts{'v'};
 # remove slashes at the end
 $Opts{'m'} =~ s{ / \z }{}gxms;
 
+# copy helper scripts to bin folder
+system("cp $Opts{'m'}/module-tools/DatabaseInstall.pl $Opts{'o'}/bin");
+system("cp $Opts{'m'}/module-tools/CodeInstall.pl $Opts{'o'}/bin");
+
 for my $Module (@ITSMModules) {
 
+    # get name of SOPM file
+    my $SOPMFile = $Module . '.sopm';
+
+    # create moule path and name with correct version
     $Module = $Opts{'m'} . '/' . $Module . $Opts{'v'};
 
+    # link the module
     system("perl $Opts{'m'}/module-tools/module-linker.pl $Opts{'a'} $Module $Opts{'o'}");
+
+    # check if DatabaseInstall and CodeInstall should be excuted
+    if ( $Opts{'a'} eq 'install' && $Opts{'d'} ) {
+        system("perl $Opts{'o'}/bin/DatabaseInstall.pl -m $SOPMFile -a install");
+        system("perl $Opts{'o'}/bin/CodeInstall.pl -m $SOPMFile -a install");
+    }
 }
 
 print "... done\n"
