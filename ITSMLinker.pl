70f9b779258356683a6061200d6c1fa92b16992d
Dominik Klein <dominik.klein@otrs.com>
2015-07-06 14:19:23 +0200

    Added the possibility to add a own path for the module tools directory.

--- ITSMLinker.pl
+++ ITSMLinker.pl
@@ -25,7 +25,7 @@ use Getopt::Std;
 
 # get options
 my %Opts = ();
-getopt( 'avmoh', \%Opts );
+getopt( 'avmoth', \%Opts );
 
 # set default
 if ( !$Opts{'a'} || !$Opts{'m'} || !$Opts{'o'} ) {
@@ -39,11 +39,12 @@ ITSMLinker.pl -  to link / unlink all ITSM modules into a OTRS system
 Copyright (C) 2001-2014 OTRS AG, http://otrs.org/
 
 Usage:
-    ITSMLinker.pl -a <install|uninstall> [ -v <ITSM branch version number> ] -m <Module-Path> -o <OTRS-path> [ -d (also executes DatabaseInstall and CodeInstall) ]
+    ITSMLinker.pl -a <install|uninstall> [ -v <ITSM branch version number> ] [ -t <Module-Tools-Path> ] -m <Module-Path> -o <OTRS-path> [ -d (also executes DatabaseInstall and CodeInstall) ]
 
 Examples:
     ITSMLinker.pl -a install -v 3.3 -m /devel -o /devel/otrs33-itsm
     ITSMLinker.pl -a install -v 4.0 -m /devel -o /devel/otrs40-itsm -d
+    ITSMLinker.pl -a install -v 4.0 -t /devel/other/module-tools -m /devel -o /devel/otrs40-itsm -d
     ITSMLinker.pl -a install -m /devel -o /devel/otrs40-itsm -d
 
 EOF
@@ -70,15 +71,25 @@ if ( $Opts{'a'} eq 'uninstall' ) {
 $Opts{'v'} ||= '';
 if ( $Opts{'v'} ) {
     $Opts{'v'} =~ s{\.}{_}gxms;
-    $Opts{'v'} = '_' . $Opts{'v'};
+    $Opts{'v'} = '-' . $Opts{'v'};
 }
 
 # remove slashes at the end
 $Opts{'m'} =~ s{ / \z }{}gxms;
 
+my $ModuleToolsPath = $Opts{'t'};
+if (!$ModuleToolsPath) {
+    $ModuleToolsPath = "$Opts{'m'}/module-tools/";
+}
+
+# remove slashes at the end
+$ModuleToolsPath =~ s{ / \z }{}gxms;
+
+
+
 # copy helper scripts to bin folder
-system("cp $Opts{'m'}/module-tools/DatabaseInstall.pl $Opts{'o'}/bin");
-system("cp $Opts{'m'}/module-tools/CodeInstall.pl $Opts{'o'}/bin");
+system("cp $ModuleToolsPath/DatabaseInstall.pl $Opts{'o'}/bin");
+system("cp $ModuleToolsPath/CodeInstall.pl $Opts{'o'}/bin");
 
 for my $Module (@ITSMModules) {
 
@@ -89,7 +100,7 @@ for my $Module (@ITSMModules) {
     $Module = $Opts{'m'} . '/' . $Module . $Opts{'v'};
 
     # link the module
-    system("perl $Opts{'m'}/module-tools/module-linker.pl $Opts{'a'} $Module $Opts{'o'}");
+    system("perl $ModuleToolsPath/module-linker.pl $Opts{'a'} $Module $Opts{'o'}");
 
     # check if DatabaseInstall and CodeInstall should be excuted
     if ( $Opts{'a'} eq 'install' && $Opts{'d'} ) {
