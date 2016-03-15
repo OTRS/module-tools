e34a880d1ea0e1f944dc98f4b831377ba208a8de
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2016-03-15 12:21:15 -0600

    Added new helpers to generate more consistent module documentation.
    -GenerateConfigChapter.pl, will replace Config2Docbook.pl

---
+++ Docbook/lib/OTRS/XML/Simple.pm
@@ -0,0 +1,48 @@
+# --
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package OTRS::XML::Simple;
+
+use strict;
+use warnings;
+
+eval { require XML::Simple };
+if ($@) {
+    print "Can't load XML::Simple: $@";
+    exit 1;
+}
+
+use base 'XML::Simple';
+
+# Override the sort method form XML::Simple
+sub sorted_keys {    ## no critic
+    my ( $Self, $Name, $Hashref ) = @_;
+
+    # only change sort order for chapter
+    if ( $Name eq 'chapter' ) {
+
+        # set the right sort order
+        my @Order = qw(title para section);
+
+        my %OrderLookup = map { $_ => 1 } @Order;
+
+        return grep { exists $Hashref->{$_} } @Order,
+            grep { !$OrderLookup{$_} } $Self->SUPER::sorted_keys( $Name, $Hashref );
+    }
+
+    # only change sort order for section
+    if ( $Name eq 'section' ) {
+
+        # set the right sort order
+        return ( 'title', 'para', );
+    }
+
+    return $Self->SUPER::sorted_keys( $Name, $Hashref );    ## no critic
+}
+
+1;
