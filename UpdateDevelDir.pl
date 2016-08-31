f05ecb1cccb5eb04e85611a6144169c7a3c91a92
Stefan Bedorf <stefan.bedorf@otrs.com>
2016-08-31 12:58:24 +0200

    Updated for changed status message of git 2.9.

--- UpdateDevelDir.pl
+++ UpdateDevelDir.pl
@@ -92,12 +92,15 @@ for my $Directory ( sort @GitDirectoryList ) {
         push @GitDirsAdeadOfRemote, $Directory;
     }
 
-    # different output between git version 1.x and 2.x
+    # different output between git versions
+    # git 2.9: nothing to commit, working tree clean
     # git 2.x: nothing to commit, working directory clean
     # git 1.x: nothing to commit (working directory clean)
     elsif (
-        $GitStatusOutput
-        =~ m{ nothing \s* to \s* commit ,? \s* \(? \s* working \s* directory \s* clean \s* \)? }xms
+        $GitStatusOutput =~ m{
+            nothing \s* to \s* commit ,?
+            \s* \(? \s* working \s* (?: directory | tree ) \s* clean \s* \)?
+        }xms
         )
     {
         push @GitDirsClean, $Directory;
