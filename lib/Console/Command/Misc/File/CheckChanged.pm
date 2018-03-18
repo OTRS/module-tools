f6c6a56b8b04f64af9049e97fe3fadd581998ac4
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2018-03-18 17:12:57 -0600

    Code cleanup.

--- lib/Console/Command/Misc/File/CheckChanged.pm
+++ lib/Console/Command/Misc/File/CheckChanged.pm
@@ -217,13 +217,3 @@ sub FindFilesOfVersion {
 }
 
 1;
-
-=head1 TERMS AND CONDITIONS
-
-This software is part of the OTRS project (L<http://otrs.org/>).
-
-This software comes with ABSOLUTELY NO WARRANTY. For details, see
-the enclosed file COPYING for license information (AGPL). If you
-did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
-
-=cut
--- lib/Console/Command/Module/File/Cleanup.pm
+++ lib/Console/Command/Module/File/Cleanup.pm
@@ -173,13 +173,3 @@ sub _Cleanup {
 }
 
 1;
-
-=head1 TERMS AND CONDITIONS
-
-This software is part of the OTRS project (L<http://otrs.org/>).
-
-This software comes with ABSOLUTELY NO WARRANTY. For details, see
-the enclosed file COPYING for license information (AGPL). If you
-did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
-
-=cut
